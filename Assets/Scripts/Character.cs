bca1e5fe60efe73220757339d425df0fa4b1fee9
sariyachanh-dotcom <sariya.chanh@bumail.net>
2025-11-04 20:04:23 +0700

    update healthbar

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,21 +1,40 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Character : MonoBehaviour
 {
-    private int health;
-    public int Health {  
+    public float health;
+    public float maxHealth;
+    public float Health
+    {  
         get { return health; } 
         set { health = (value < 0) ? 0 : value ; }  
-    
+    }
+    public float MaxHealth
+    {
+        get { return maxHealth; }
+        set { maxHealth = (value < 0) ? 0 : value; }
     }
 
     protected Animator anim ;
     protected Rigidbody2D rb;
 
+    public Image healthBar;
+
+
+    public void UpdateHealthBarUI()
+    {
+        float fillAmount = Mathf.Clamp01(Health / MaxHealth);
+        healthBar.fillAmount = fillAmount;
+    }
+
     public void Initialized(int startHealth)
     {
         Health = startHealth;
-        Debug.Log($"{this.name} is Initialed Health : {this.Health}");
+        MaxHealth = startHealth;
+        Debug.Log($"{this.name} is Initialized with Health : {this.MaxHealth}");
+        if (healthBar != null )
+            UpdateHealthBarUI();
 
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
@@ -24,13 +43,15 @@ public class Character : MonoBehaviour
     public void TakeDamage(int damage)
     {
         Health -= damage;
-        Debug.Log($"{this.name} took damage {damage} Current Health : {Health}");
+        Debug.Log($"{this.name} took damage {damage}, Current Health : {Health} / {MaxHealth}");
+        if (healthBar != null)
+            UpdateHealthBarUI();
 
         IsDead();
     }
     public bool IsDead()
     {
-        if(health <= 0)
+        if (Health <= 0)
         {
             Destroy(this.gameObject);
             return true;
---
+++ Assets/Scripts/WorldHealthBar.cs
@@ -0,0 +1,36 @@
+﻿/*using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldHealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private Character target; //ตัวละครที่ Health Bar
+    [SerializeField] private Vector3 offset = new Vector3(0, 2f, 0);
+
+    private Camera mainCam;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+        if (target != null)
+        {
+            slider.maxValue = target.Health;
+            slider.value = target.Health;
+        }
+    }
+
+    void Update()
+    {
+        if (target == null) return;
+
+        //อัปเดตค่าเลือด
+        slider.value = target.Health;
+
+        //หมุนตามกล้องหันเข้าหน้าผู้เล่น
+        transform.rotation = Quaternion.LookRotation(transform.position - mainCam.transform.position);
+
+        //ติดหัวตัวละคร
+        transform.position = target.transform.position + offset;
+    }
+}
+*/
